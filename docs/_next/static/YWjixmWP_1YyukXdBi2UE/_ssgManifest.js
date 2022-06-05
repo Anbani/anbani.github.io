70970ae77a1fe5f349823e68dbe71a6e290a63e5
George Gach <georgegach@outlook.com>
2022-06-05 16:14:44 +0400

    Monospace font fix

---
+++ docs/_next/static/YWjixmWP_1YyukXdBi2UE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002F404","\u002F[locale]\u002Fcalendar","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fkeyboard","\u002F[locale]\u002Flearn","\u002F[locale]\u002Ftextart","\u002F[locale]\u002Fconverter","\u002F[locale]\u002Fconverter\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
