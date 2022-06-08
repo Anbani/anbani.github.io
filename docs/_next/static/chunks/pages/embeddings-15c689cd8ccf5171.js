8bd71532a099be091cb7ae14e44da6a34c50adc1
George Gach <georgegach@outlook.com>
2022-06-08 18:59:05 +0400

    Update

---
+++ docs/_next/static/chunks/pages/embeddings-15c689cd8ccf5171.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[699],{7041:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/embeddings",function(){return _(8699)}])},8699:function(n,u,_){"use strict";_.r(u);var e=_(6834);u.default=e.l_}},function(n){n.O(0,[834,774,888,179],(function(){return u=7041,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
