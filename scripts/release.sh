3637e5fb6aa548b1f47ccd8d534de43a8f0b0d12
George Gach <georgegach@outlook.com>
2025-04-04 20:46:30 +0400

    🚀 Upgrade

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,15 +6,40 @@
 # ▄▄  ██ ██  ██ ██  ██ ▄▄  ██ ██  ██ ██  ██ 
 # ██████ ██████ ██████ ██████ ██████ ██  ██ 
 
+# Usage:
+#   ./deploy.sh [production_directory] [development_directory]
+#
+# Example:
+#   ./deploy.sh /path/to/prod/ /path/to/dev/
+#
+# If no parameters are passed, the following defaults will be used:
+production_directory=${1:-'/home/george/Space/GitHub/anbani.github.io'}
+development_directory=${2:-'/home/george/Space/GitHub/anbani-next'}
 
-production_directory='/home/george/Dev/anbani.github.io/'
-development_directory='/home/george/Dev/anbani-next/'
-
+echo "Production directory: $production_directory"
+echo "Development directory: $development_directory"
 
 # Clean production static hosting directory
-echo '჻ Pulling latest gh build' && cd $production_directory && git pull 
-echo '჻ Cleaning up production static hosting directory' && rm -rf 'docs/'
+echo '჻ Pulling latest gh build'
+cd "$production_directory" || exit 1
+git pull 
+
+echo '჻ Cleaning up production static hosting directory'
+rm -rf docs/
+
 # Deploy development build from next.anbani.ge
-echo '჻ Copying latest development build' && cd $development_directory && cp -r 'docs' $production_directory
-echo '჻ Fixing CNAME'  && cd $production_directory && cd 'docs' && echo 'anbani.ge' > CNAME
-echo '჻ Creating update git commit'  && cd $production_directory && git add . && git commit -m "🚀 Upgrade" && git push && echo '჻ All done!'
+echo '჻ Copying latest development build'
+cd "$development_directory" || exit 1
+cp -r docs "$production_directory"
+
+echo '჻ Fixing CNAME'
+cd "$production_directory/docs" || exit 1
+echo 'anbani.ge' > CNAME
+
+echo '჻ Creating update git commit'
+cd "$production_directory" || exit 1
+git add .
+git commit -m "🚀 Upgrade"
+git push
+
+echo '჻ All done!'
