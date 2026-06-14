8e0c9e0b8eb13397c37c55cd62de8e41c04c2c93
George Gach <georgegach@outlook.com>
2026-06-15 00:17:46 +0400

    Deploy: adaptive spotlight (illuminate dark / subtle tint light) (from anbani-next 4b265b1)

--- docs/_next/static/chunks/1bvokga9n6q34.js
+++ docs/_next/static/chunks/1bvokga9n6q34.js
@@ -4,6 +4,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/2pflrp5zaqpfv.js",
   "static/chunks/1ngra23y-ka96.js",
   "static/chunks/2gnbr86za9jo7.js",
-  "static/chunks/2-xrpnckzb00q.css",
-  "static/chunks/turbopack-0b9yygnxrlhsl.js"
+  "static/chunks/2qdfrx7u5z1_j.css",
+  "static/chunks/turbopack-27ms9toj19grl.js"
 ])
