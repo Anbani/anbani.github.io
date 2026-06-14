19ae7b4834b0dadc96665de3ebcfb579f6aea5ad
George Gach <georgegach@outlook.com>
2026-06-15 00:37:10 +0400

    Deploy: spotlight perf fix (transform + rAF) (from anbani-next a03b6de)

--- docs/_next/static/chunks/3ddcxg6hm3thb.js
+++ docs/_next/static/chunks/3ddcxg6hm3thb.js
@@ -1,9 +1,9 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/2kv7dg6mms6w2.js",
   "static/chunks/3fc7vm-q13cox.js",
-  "static/chunks/2pflrp5zaqpfv.js",
+  "static/chunks/0xhzjts_va-u_.js",
   "static/chunks/1ngra23y-ka96.js",
   "static/chunks/2gnbr86za9jo7.js",
-  "static/chunks/2qdfrx7u5z1_j.css",
-  "static/chunks/turbopack-27ms9toj19grl.js"
+  "static/chunks/3rbbv3miytejk.css",
+  "static/chunks/turbopack-41qkq-b87ykia.js"
 ])
